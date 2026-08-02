02be860a942bfa23ef43889792019ef4a2daf36e
SANDYYYY <sandeshlamsal8@gmail.com>
2026-08-02 14:13:22 +0545

    commit new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tickets',
+    'corsheader',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheader.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
