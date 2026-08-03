ebb2a4075ead8990e00679fe53c6e52e85096f13
SANDYYYY <sandeshlamsal8@gmail.com>
2026-08-03 14:03:39 +0545

    configuring jwt creating jwt endpoint create user profile model made permission classes created decision log model and created superuser for admin pannel

---
+++ backend/tickets/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework import permissions
+
+
+class IsAdmin(permissions.BasePermission):
+    """
+    Only admin users can access.
+    """
+
+    def has_permission(self, request, view):
+        return (
+            request.user and
+            request.user.is_authenticated and
+            hasattr(request.user, 'profile') and
+            request.user.profile.role == 'admin'
+        )
+
+
+class IsAgent(permissions.BasePermission):
+    """
+    Only agent users (or admin) can access.
+    """
+
+    def has_permission(self, request, view):
+        return (
+            request.user and
+            request.user.is_authenticated and
+            hasattr(request.user, 'profile') and
+            request.user.profile.role in ['agent', 'admin']
+        )
